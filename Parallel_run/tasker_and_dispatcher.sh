ce82287896705e4c6c3f48103b006926aa6bdcd5
IdanSeg <idansegaldev@gmail.com>
2025-02-08 08:57:20 +0200

    increase tasker job allocated time

--- Parallel_run/tasker_and_dispatcher.sh
+++ Parallel_run/tasker_and_dispatcher.sh
@@ -43,7 +43,7 @@ echo "Submitting tasker job"
 # Run the tasker as sbatch job
 job_id=$(sbatch --parsable << EOF
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --output=slurm-%j.out
