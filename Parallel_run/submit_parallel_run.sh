af1a1b4b3615649523f88bf0ce4326b9990b3a54
IdanSeg <idansegaldev@gmail.com>
2025-02-08 09:47:02 +0200

    make tasker_and_dispatcher.sh an SLURM job

--- Parallel_run/submit_parallel_run.sh
+++ Parallel_run/submit_parallel_run.sh
@@ -1,3 +1,8 @@
+# Load shared configuration
+source ~/.config/annoTransfer.conf || exit 1
+: "${PROJECT_DIR:?}" "${VENV_NAME:?}" "${TMP_DIR:?}" "${CACHE_DIR:?}"
+
+# run tasker and dispatcher
 job_id=$(sbatch $PROJECT_DIR/Parallel_run/tasker_and_dispatcher.sh | awk '{print $4}') && \
 echo "Tasker job submitted with ID: $job_id" && \
 tail -F main_controller.out main_controller.err
