7e2e9e7df6aaae10ff7bc5f58c92418ba197661f
IdanSeg <idansegaldev@gmail.com>
2025-02-11 08:06:06 +0200

    fix redudent missing controller log print

--- Parallel_run/submit_parallel_run.sh
+++ Parallel_run/submit_parallel_run.sh
@@ -6,5 +6,12 @@ source ~/.config/annoTransfer.conf || exit 1
 
 # run tasker and dispatcher
 job_id=$(sbatch $PROJECT_DIR/Parallel_run/tasker_and_dispatcher.sh | awk '{print $4}') && \
-echo "Tasker job submitted with ID: $job_id" && \
+echo "Tasker job submitted with ID: $job_id"
+
+# Wait for SLURM files to be created
+while [ ! -f "main_controller_${job_id}.out" ] || [ ! -f "main_controller_${job_id}.err" ]; do
+    sleep 1
+done
+
+# Start tailing after files exist
 tail -F main_controller_$job_id.out main_controller_$job_id.err
